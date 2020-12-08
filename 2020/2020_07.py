a6cdee8dc3457664df961c137d9ee8a6f23155f4
Sam Shideler <35845288+sjshide@users.noreply.github.com>
2020-12-07 17:02:05 -0700

    Update 2020_07.py

--- 2020/2020_07.py
+++ 2020/2020_07.py
@@ -4,6 +4,7 @@ from utils import *
 ## Part B tooke me like 2 hours live. Sloppiness with how I was keeping track
 ## of multiplying throughout the process led to very large answers for awhile
 ## Then essentially rewrote from scratch in the dumbest way possible to get the right answer
+## (writing down all chains from 'shiny gold' to a final bag and summing over each of these)
 ## The below is my attempt with a fresh mind the next day to doing something a little cleaner
 ## (without changing how I did the parsing/part A).
 ## Still pretty unclean, but better than the trash I wrote live.
@@ -55,7 +56,6 @@ for n in list(adj.keys()):
 
 
 def total_bag_ct(node):
-    #print(node)
     bag_name = node[0]
     mult = node[1]
     return(mult*(1+sum([total_bag_ct(nodes) for nodes in adj[bag_name]])))
