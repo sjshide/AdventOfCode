70167e10a92e835ab98b149ad9561224dacefeb0
Sam Shideler <35845288+sjshide@users.noreply.github.com>
2021-12-08 22:39:27 -0700

    Add files via upload

--- 2021/2021_09.py
+++ 2021/2021_09.py
@@ -3,45 +3,35 @@ import networkx as nx
 
 inp = get_input(2021,9)
 
-d=dict()
-for i in range(len(inp)):
-    for j in range(len(inp[i])):
-        d[(i,j)]=int(inp[i][j])
-
-    
-# A
-A = 0
-
-for (i,j) in d:
-    nbct = 0
-    ct = 0
-    for (dy,dx) in [(1,0),(-1,0),(0,1),(0,-1)]:
-        if (i+dy,j+dx) in d:
-            nbct+=1
-            if d[(i+dy,j+dx)]>d[(i,j)]:
-                ct+=1
-    if ct==nbct:
-        A+=(1+d[(i,j)])
-    
-        
-# B
-B = 1
-
 G = nx.Graph()
 
+A = 0
+B = 1
+
+for i in range(len(inp)):
+    for j in range(len(inp)):
+        G.add_node((i,j))
+        
+for (i,j) in G:
+    ct = 0
+    nbct = 0
+    for (di,dj) in [(-1,0),(1,0),(0,-1),(0,1)]:
+        if (i+di,j+dj) in G:
+            G.add_edge((i+di,j+dj),(i,j))
+            nbct+=1
+            if d[(i+di,j+dj)]>d[(i,j)]:
+                ct+=1
+    if ct==nbct:
+        A+=(1+int(inp[i][j]))
+
+        
 for i in range(len(inp)):
     for j in range(len(inp[i])):
-        if d[(i,j)]!=9:
-            G.add_node((i,j))
-
-for (i,j) in G:
-    for (dy,dx) in [(1,0),(-1,0),(0,1),(0,-1)]:
-        if (i+dy,j+dx) in G:
-            G.add_edge((i+dy,j+dx),(i,j))
-
-
+        if inp[i][j]=='9':
+            G.remove_node((i,j))
+            
 for l in sorted(([len(x) for x in list(nx.connected_components(G))]))[-3:]:
-    B*=l  
+    B*=l
 
 print('Part A Solution:', A)
 print('Part B Solution:', B)
